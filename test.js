65609de15232ccc8fd0eebbf46e7a4bcc42ad104
David Lasky <dclasky@gmail.com>
2022-06-30 17:01:01 -0700

    properly remade js colorboxes

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
-alert("FART");
+
+// function round() {
+//     document.getElementById('square').setAttribute("class",)
+// }
+
+
+
+window.onload = function addBoxes() {
+    for (let i = 0; i < 400; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("box");
+        newDiv.setAttribute('id', i)
+        newDiv.setAttribute('onmouseover', 'colorchange(' + i + ')')
+        document.getElementById("container").appendChild(newDiv);
+    }
+}
+
+function colorchange(boxId) {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    document.getElementById(boxId).style.backgroundColor = ["rgb(", r, ",", g, ",", b, ")"].join("");
+}
