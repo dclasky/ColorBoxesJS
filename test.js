f644b72cfe1f359d84499a0e1bc0ce5f92b1ff20
David Lasky <dclasky@gmail.com>
2022-07-01 10:33:00 -0700

    some nudges I think

--- test.js
+++ test.js
@@ -13,19 +13,22 @@ window.onload = function addBoxes() {
 //a box that's moused over gets randomized rgb values, and then full opacity(a).  
 //then the opacity erodes over a set interval 
 //NOTE:boxes that have been changed once flicker if moused over a second time
+
 function colorchange(boxId) {
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
-    var a = 1;
+    var a = 1.0;
     document.getElementById(boxId).style.backgroundColor = ["rgba(", r, ",", g, ",", b, ",", a, ")"].join("");
-    setInterval(function () {
-        // while (a > 0) {
-        a -= .01;
+
+    setInterval(function decay() {
+        //while (a > 0.0) {
+        a = a - .01;
         //}
         document.getElementById(boxId).style.backgroundColor = ["rgba(", r, ",", g, ",", b, ",", a, ")"].join("");
-    }, 10);
+    }, 1);
 }
+
 
 //make squares have opacity
 //make their opacity fade
